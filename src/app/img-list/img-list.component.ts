c9b445a82fd75129ea1206ee51b89e05d9bf8e74
Valorad <wcxaaa@gmail.com>
2017-04-19 10:50:35 +0800

    * optimized datepicker

--- src/app/img-list/img-list.component.ts
+++ src/app/img-list/img-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 
+interface IPicker {
+  shown: boolean
+}
+
 @Component({
   selector: 'app-img-list',
   templateUrl: './img-list.component.html',
@@ -15,7 +19,35 @@ export class ImgListComponent implements OnInit {
   ngOnInit() {
   }
 
+  /* DatePicker settings start */
+
   dateFrom: any;
   dateTo: any;
+
+
+  modalShown: boolean = false;
+
+  dtf: IPicker = {
+    shown: false
+  };
+
+  dtt: IPicker = {
+    shown: false
+  };
+
+  togglePicker(picker: IPicker) {
+    picker.shown = !picker.shown;
+    this.modalShown = !this.modalShown;
+  }
+
+  closeAllPickers() {
+    this.dtf.shown = false;
+    this.dtt.shown = false;
+    this.modalShown = false;
+  }
+
+  /* DatePicker settings end */
+
+
 
 }
