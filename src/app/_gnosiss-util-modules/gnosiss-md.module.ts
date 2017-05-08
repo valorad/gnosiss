e9e68ab253c76d50c8330a8b52de18375ee28b44
Valorad <wcxaaa@gmail.com>
2017-05-08 22:19:49 +0800

    + basic info input

--- src/app/_gnosiss-util-modules/gnosiss-md.module.ts
+++ src/app/_gnosiss-util-modules/gnosiss-md.module.ts
@@ -9,7 +9,8 @@ import {
   MdInputModule,
   MdSelectModule,
   MdTabsModule,
-  MdProgressBarModule
+  MdProgressBarModule,
+  MdSnackBarModule
 } from '@angular/material';
 
 @NgModule({
@@ -22,7 +23,8 @@ import {
     MdInputModule,
     MdSelectModule,
     MdTabsModule,
-    MdProgressBarModule
+    MdProgressBarModule,
+    MdSnackBarModule
   ],
   exports: [
     MdButtonModule,
@@ -32,7 +34,8 @@ import {
     MdInputModule,
     MdSelectModule,
     MdTabsModule,
-    MdProgressBarModule
+    MdProgressBarModule,
+    MdSnackBarModule
   ]
 })
 export class GnosissMdModule { }
--- src/app/img-upload/img-upload.component.ts
+++ src/app/img-upload/img-upload.component.ts
@@ -4,6 +4,8 @@ import { FileUploader } from 'ng2-file-upload';
 
 import { GnosissUploader } from './gnosissUploader.class';
 
+import { MdSnackBar } from '@angular/material';
+
 @Component({
   selector: 'gnosiss-img-upload',
   templateUrl: './img-upload.component.html',
@@ -11,32 +13,131 @@ import { GnosissUploader } from './gnosissUploader.class';
 })
 export class ImgUploadComponent implements OnInit {
 
-  constructor() { }
+  constructor( public snackBar: MdSnackBar ) { }
 
   ngOnInit() {
-    this.imgUploader.uploader.onWhenAddingFileFailed = () => {
-      this.alertFailed();
+    this.imgUploader.uploader.onWhenAddingFileFailed = (item: any, filter: any, options: any) => {
+
+      this.alertFailed(item.name);
     };
+
+    this.thumbnailUploader.uploader.onWhenAddingFileFailed = (item: any, filter: any, options: any) => {
+
+      this.alertFailed(item.name);
+    };
+
+    this.imgUploader.uploader.onAfterAddingFile = (fileItem: any) => {
+
+      this.extractFileName(fileItem.file.name);
+    };
+
+
   }
 
   private imgUploader: GnosissUploader = new GnosissUploader(
     {
       url: "string",
-      allowedMimeType:["application/gzip","application/tar+gzip", "application/tar"]
+      queueLimit: "1"
     } 
   );
 
-  private thumbnailUploader = new GnosissUploader(
+  private thumbnailUploader: GnosissUploader = new GnosissUploader(
     {
       url: "string",
       allowedMimeType:["image/jpeg", "image/png"]
     } 
   );
 
-  private alertFailed = () => {
-    window.alert("failed to add");
-    console.log("failed to add");
+  private alertFailed: any = (item: string) => {
+    this.openSnackBar(`${item} failed to add`, "Pity");
   };
-  
 
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  private extractFileName: any = (filename: string) => {
+    let strList = filename.split(".");
+    this.rsImg.name = strList[0];
+    let extension = "";
+    for (let i = 1; i < strList.length; i++) {
+      extension += strList[i];
+      if (i < strList.length - 1) {
+        extension += ".";
+      }
+    }
+    this.rsImg.extension = extension;
+  };
+
+  private formData: any = async () => {
+    console.log(this.rsImg);
+
+    // upload data
+    await this.uploadImgThumbnails();
+
+    if (this.imgUploader.uploader.queue.length <= 0) {
+      this.openSnackBar(`You haven't specified an img to upload`, "Okay");
+      return;
+    }
+
+    if (this.imgUploader.uploader.queue[0].isError) {
+      this.openSnackBar(`There was an error uploading img`, "Opps");
+      return;
+    }
+
+    // link thumbnail
+    let thumbnails: [string] = [""];
+      // clear array
+    thumbnails.length = 0;
+
+    for (let item of this.thumbnailUploader.uploader.queue) {
+      if (item.isSuccess) {
+        thumbnails.push(item.file.name);
+      }
+    }
+
+    // assign value to local
+    this.thumbnails.thumbnails = thumbnails;
+
+    console.log(this.thumbnails);
+  };
+
+  private uploadImgThumbnails: any = async () => {
+    await this.imgUploader.uploader.uploadAll();
+    await this.thumbnailUploader.uploader.uploadAll()
+  };
+
+  private rsImg: any = {
+    name: "",
+    extension: "",
+    basicInfo: {
+        satellite: "",
+        dataType: "",
+        sensor: "",
+        time: "",
+        stripeNo: 0,
+        rowNo: 0,
+        SEA: 0,
+        azimuth: 0,
+        observatory: ""
+    },
+    dateInfo: {
+        timeAcquired: "",
+        timeEnd: ""
+    },
+    spatialInfo: {
+        latitude: 0,
+        longitude: 0
+    }
+  }
+
+  private thumbnails: any = {
+      name: "",
+      thumbnails: [
+        ""
+      ]
+  }
+  
 }
